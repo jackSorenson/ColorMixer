e44eda6a04ace3decec51c171e09205f17c9643b
jackSorenson <71714898+jackSorenson@users.noreply.github.com>
2020-11-06 09:13:46 -0500

    Add files via upload

---
+++ ColorMixer/ColorMixer/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  ColorMixer
+//
+//  Created by Jack Sorenson on 11/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        if redSwitch.isOn {
+            red = CGFloat(redSlider.value)
+        }
+
+        if greenSwitch.isOn {
+            green = CGFloat(greenSlider.value)
+        }
+        if blueSwitch.isOn {
+            blue = CGFloat(blueSlider.value)
+        }
+        
+        let color = UIColor(red: red, green: green,
+           blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
+
+
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       updateColor()
+        // Do any additional setup after loading the view.
+        colorView.layer.borderWidth = 5
+        colorView.layer.cornerRadius = 20
+        colorView.layer.borderColor =
+            UIColor.black.cgColor
+    }
+
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        updateColor()
+        updateControls()
+    }
+
+
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        updateColor()
+    }
+ 
+    @IBAction func reset(_ sender: Any) {
+        redSlider.value = 1
+        redSwitch.isOn = false
+        greenSlider.value = 1
+        greenSwitch.isOn = false
+        blueSlider.value = 1
+        blueSwitch.isOn = false
+        updateColor()
+    }
+    
+    func updateControls() {
+            redSlider.isEnabled = redSwitch.isOn
+            greenSlider.isEnabled = greenSwitch.isOn
+            blueSlider.isEnabled = blueSwitch.isOn
+    }
+}
+
